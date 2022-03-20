a681f398428d3075a1ed5b861f95c546b4dc72f8
sazal <sazal@itconquest.com>
2022-03-20 20:02:13 +0600

    first commit

--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import store from './store'
+import router from './router'
+import './index.css'
 
-createApp(App).mount('#app')
+createApp(App)
+.use(store)
+.use(router)
+.mount('#app')
+// 25 min 33sec
---
+++ src/store/index.js
@@ -0,0 +1,18 @@
+import { createStore } from "vuex";
+
+const store = createStore({
+    state: {
+        user: {
+            data: {
+                name: 'Sazal'
+            },
+            token:null
+        }
+    },
+    mutations: {},
+    actions: {},
+    getters: {},
+    modules: {}
+})
+
+export default store;
